20241b9d247106e1566612c59efc0bf5014ef3ba
Orazio <orazio.narducci@gmail.com>
2026-09-20 11:00:02 -0300

    fix: remover imports CSS legados do AG Grid

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js";
+// import 'ag-grid-community/styles/ag-grid.css'
+// import 'ag-grid-community/styles/ag-theme-alpine.css'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).mount("#app");
